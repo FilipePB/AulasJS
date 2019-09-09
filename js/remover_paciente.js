17d33fb61871cd12ecf14cabaaa0ae85bddf93a6
FilipePB <filipepbregolim@gmail.com>
2019-09-09 13:42:09 -0300

    continuação da aula

--- js/remover_paciente.js
+++ js/remover_paciente.js
@@ -1,8 +1,9 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick",function(){
-        //o this puxa o valor clicado 
-        this.remove(); //função de remoção
-    });
-})
+tabela.addEventListener("dblclick",function(event){
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500)
+});
